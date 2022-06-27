e955c3dfdf1836b940a4fb78b5e0ac92e85f0cbb
HM30082005 <haretonenko.misha@gmail.com>
2022-06-26 20:08:41 -0700

    Parcing from json

--- RBI-lite/ViewController.swift
+++ RBI-lite/ViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    struct recipe: Decodable {
+        let id: Int
+        let title: String
+        let description: String
+        let ingridients: [String]
+        let directions: String
+    }
+    
+    private func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+    
+    private func parse(jsonData: Data) {
+        guard let recipes = try? JSONDecoder().decode([recipe].self, from: jsonData) else {
+            print("Decode Error")
+            return
+        }
+        print(recipes)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let localData = self.readLocalFile(forName: "recipes") {
+            self.parse(jsonData: localData)
+        }
     }
 
 
